b8cc3a9550bd34b377f9e0061d26f0ef67a24903
Tahir Naushasd <t.naushad25@gmail.com>
2017-08-25 13:12:38 +0100

    asp.net core 2.0

--- Fiver.Mvc.TagHelpers.Custom/Startup.cs
+++ Fiver.Mvc.TagHelpers.Custom/Startup.cs
@@ -1,37 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using Fiver.Mvc.TagHelpers.Custom.Models.Home;
 
 namespace Fiver.Mvc.TagHelpers.Custom
 {
     public class Startup
     {
-        public Startup(
-            IHostingEnvironment env,
-            ILoggerFactory loggerFactory)
-        {
-
-        }
-
         public void ConfigureServices(
             IServiceCollection services)
         {
             services.AddScoped<IGreetingService, GreetingService>();
-
             services.AddMvc();
         }
 
         public void Configure(
             IApplicationBuilder app, 
-            IHostingEnvironment env, 
-            ILoggerFactory loggerFactory)
+            IHostingEnvironment env)
         {
             app.UseDeveloperExceptionPage();
             app.UseMvc(routes =>
